9f839c11e170ec0f8106ca1d274796c5d0b9a42f
skyblood <segrojased@gmail.com>
2017-09-08 20:51:30 -0500

    Fase 1

---
+++ serverHTTP.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+const puerto = 8080;
+
+app.get('/', function(req, res){
+	res.sendFile('view/index.html');
+	console.log('Se conecto alguien');
+	//respuesta.end();
+});
+
+app.listen(puerto, ()=> {console.log('Se arranco el server en el puerto '+puerto)});
